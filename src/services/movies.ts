6cf0873b0f67190499a6906ad73d2f761494a6c2
alexLahady <alexandrelahady@gmail.com>
2025-02-16 19:22:59 +0100

    find first movie

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,3 +1,5 @@
+import { prisma } from "./prisma"
+
 export class MoviesService {
     public static getAll() {
         return [{
@@ -10,11 +12,9 @@ export class MoviesService {
         },]
     }
 
-    public static getOne() {
-        return {
-            id: 46,
-            title: "Avatar - Voie de l'eau"
-        }
+    public static async getOne() {
+        const oneMovie =  await prisma.movies.findFirst();
+        return oneMovie;
     }
 
     public static getByUser(userId: string) {
