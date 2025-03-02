d7bdc7a124cc137af6bf254abc30168587f1cc11
alexLahady <alexandrelahady@gmail.com>
2025-03-02 12:01:55 +0100

    Petite correction

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,25 @@
 
 */
 
-// pas un ts mais à utiiliser dans un fichier sample html pour les test
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const apiKey = process.env.API_KEY; 
+
+async function fetchData() {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/movie/11', {
+      headers: {
+        'Authorization': `Bearer ${apiKey}` 
+      }
+    });
+    
+    console.log(response.data); 
+  } catch (error) {
+    console.error('Erreur:', error); 
+  }
+}
+
+fetchData();
