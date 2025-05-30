9adc7e2cd44052794c28fff62c12cc1b5f372dff
alexLahady <alexandrelahady@gmail.com>
2025-05-30 12:06:53 +0200

    Amélioration de l'api, il peut afficher 100 films au lieu de 20

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,9 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const router = express.Router();
 
-const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
-
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY_MOVIEDB;
 
 const options: AxiosRequestConfig = {
     headers: {
@@ -17,29 +15,44 @@ const options: AxiosRequestConfig = {
     }
 };
 
-router.get('/', (req, res) => {
-    axios
-        .get(url, options)
-        .then(response => {
-            // Filtrer les données pour ne garder que l'essentiel
+type Movie = {
+    id: number;
+    title: string;
+    release_date: string;
+    overview: string;
+    vote_average: number;
+};
+
+router.get('/', async (req, res) => {
+    try {
+        let allResults: Movie[] = [];
+
+        for (let i = 1; i <= 5; i++) {
+            const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${i}`;
+
+            const response = await axios.get(url, options);
+
             const filteredData = response.data.results.map(movie => ({
                 id: movie.id,
                 title: movie.title,
                 release_date: movie.release_date,
                 overview: movie.overview,
-                vote_average: movie.vote_average, 
+                vote_average: movie.vote_average,
             }));
-            //console.log(filteredData);
-            res.send(filteredData);
-            //console.log(response.data)
-            //res.send(response.data);
-        })
 
+            // Permet d'ajouter que la valeur du tableau quand un tableau est deja créer 
+            // et peu rajouter par dessus comme un push sans qu'il devient un tableau
+            allResults = allResults.concat(filteredData);
+            //console.log(allResults);
+        }
 
-        .catch(error => {
-            console.log(error);
-        })
-})
+        res.send(allResults);
+    } catch (error) {
+        console.error("Erreur API TMDB :", error);
+        res.status(500).send("Erreur de récupération des films");
+    }
+});
+
 
 export default router;
 
@@ -49,3 +62,4 @@ pour afficher plus de 20 éléments on fera :
  - une fonction url qui sera une boucle sur la partie 'page'
  - améliorer la route pour afficher au moins 100 élément avec la nouvelle fonction url 
 */
+
