0ee52735d3cca20b4327cd8f05a0993994d22588
alexLahady <alexandrelahady@gmail.com>
2025-06-10 17:58:44 +0200

    access control avtivé

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,18 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server caca");
 });
 */
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", 'true');
+  res.setHeader("Access-Control-Allow-Origin", 'https://movie-test-vercel-delta.vercel.app');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+
+
 app.get("/", async (req: Request, res: Response) => {
   const user = [{
     id: 0,
@@ -32,6 +44,26 @@ app.get("/", async (req: Request, res: Response) => {
 
   res.send(user);
 });
-
+app.listen(3001, () => console.log("Server ready on port 3001."));
 
 module.exports = app;
+
+/*
+//test vercel avec ça 
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "src/index.js",
+      "use": "@vercel/node",
+      "config": { "includeFiles": ["src/**"] }
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "src/index.js"
+    }
+  ]
+}
+*/
